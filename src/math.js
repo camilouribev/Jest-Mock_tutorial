c15044bec1e75e2961c7e76dbbed5e122a514403
Camilo U <cauribeva@unal.edu.co>
2021-10-14 12:36:14 -0500

    Corrected math module

--- src/math.js
+++ src/math.js
@@ -1,5 +1,4 @@
-import * as math from "./math.js";
-export const doAdd = (a, b) => math.add(a, b);
-export const doSubtract = (a, b) => math.subtract(a, b);
-export const doMultiply = (a, b) => math.multiply(a, b);
-export const doDivide = (a, b) => math.divide(a, b);
+export const add = (a, b) => a + b;
+export const subtract = (a, b) => b - a;
+export const multiply = (a, b) => a * b;
+export const divide = (a, b) => b / a;
